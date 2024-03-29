0e3e750d494f2048d21fb230db2817af193f4d34
PrahilBhowmik <mebhowmik321@gmail.com>
2024-03-29 15:03:18 +0530

    Initial Commit

---
+++ foodTrain.h
@@ -0,0 +1,5 @@
+#pragma once 
+#include "./include/k2c_tensor_include.h" 
+void foodTrain(k2c_tensor* input_1_input, k2c_tensor* activation_output); 
+void foodTrain_initialize(); 
+void foodTrain_terminate(); 
